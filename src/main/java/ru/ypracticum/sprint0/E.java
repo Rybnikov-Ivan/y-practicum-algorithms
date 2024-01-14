ce36bbeccc9e809f0c3ebc66bda53e70347ea25d
Ivan Rybnikov <ivan.rybnikov@rt.ru>
2024-01-14 17:07:40 +0300

    #1 E

---
+++ src/main/java/ru/ypracticum/sprint0/E.java
@@ -0,0 +1,60 @@
+package ru.ypracticum.sprint0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class E {
+
+    // Если ответ существует, верните список из двух элементов
+    // Если нет - то верните пустой список
+    private static List<Integer> twoSum(List<Integer> arr, int targetSum) {
+        Collections.sort(arr);
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            int a = arr.get(left);
+            int b = arr.get(right);
+            int sum = a + b;
+            if (sum == targetSum) {
+                return List.of(a, b);
+            } else if (sum > targetSum) {
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+
+        return Collections.emptyList();
+    }
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = readInt(reader);
+            List<Integer> arr = readList(reader);
+            int targetSum = readInt(reader);
+            List<Integer> result = twoSum(arr, targetSum);
+            if (result.isEmpty()) {
+                System.out.println("None");
+            } else {
+                System.out.println(result.get(0) + " " + result.get(1));
+            }
+        }
+    }
+
+    private static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return  Arrays.asList(reader.readLine().split(" "))
+                .stream()
+                .map(elem -> Integer.parseInt(elem))
+                .collect(Collectors.toList());
+    }
+}
